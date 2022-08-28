3a0b2e3b3c24b0495d9eaa4e2094d5a14b2dd67b
Kaan Ozdogan <48806820+Lethalos@users.noreply.github.com>
2022-08-28 13:06:22 +0300

    Update script.js

--- Rock_Paper_Scissors/script.js
+++ Rock_Paper_Scissors/script.js
@@ -1,46 +1,56 @@
+const scores = { playerScore: 0, computerScore: 0 };
+
 function getComputerChoice() {
+  const choices = ["Rock", "Paper", "Scissors"];
   const randomNum = Math.floor(Math.random() * 3);
 
-  if (randomNum == 0) {
-    return "Rock";
-  } else if (randomNum == 1) {
-    return "Paper";
-  } else if (randomNum == 2) {
-    return "Scissors";
-  }
+  return choices[randomNum];
 }
 
 function getResult(playerChoice, computerChoice) {
-  if (playerChoice == computerChoice) return 0;
-  else if (playerChoice == "Rock" && computerChoice == "Scissors") return 1;
-  else if (playerChoice == "Paper" && computerChoice == "Rock") return 1;
-  else if (playerChoice == "Scissors" && computerChoice == "Paper") return 1;
-  else if (playerChoice == "Scissors" && computerChoice == "Rock") return -1;
-  else if (playerChoice == "Rock" && computerChoice == "Paper") return -1;
-  else if (playerChoice == "Paper" && computerChoice == "Scissors") return -1;
+  let score;
+  if (playerChoice == computerChoice) score = 0;
+  else if (playerChoice == "Rock" && computerChoice == "Scissors") score = 1;
+  else if (playerChoice == "Paper" && computerChoice == "Rock") score = 1;
+  else if (playerChoice == "Scissors" && computerChoice == "Paper") score = 1;
+  else if (playerChoice == "Scissors" && computerChoice == "Rock") score = -1;
+  else if (playerChoice == "Rock" && computerChoice == "Paper") score = -1;
+  else if (playerChoice == "Paper" && computerChoice == "Scissors") score = -1;
+
+  return score;
 }
 
 function showResult(score, playerChoice, computerChoice) {
+  const playerScoreDiv = document.getElementById("player-score");
+  const handsDiv = document.getElementById("hands");
   const resultDiv = document.getElementById("result");
 
-  if (score == 1)
-    resultDiv.innerText = `Player: ${playerChoice}\nComputer: ${computerChoice}\nYou Win!`;
-  if (score == 0)
-    resultDiv.innerText = `Player: ${playerChoice}\nComputer: ${computerChoice}\nIt's a Draw`;
-  if (score == -1)
-    resultDiv.innerText = `Player: ${playerChoice}\nComputer: ${computerChoice}\nYou Lose :*(`;
+  handsDiv.innerText = `😉 ${playerChoice} vs 🤖 ${computerChoice}`;
+
+  if (score == 1) {
+    scores.playerScore++;
+    resultDiv.innerText = "You Win! 🎉";
+  }
+  if (score == 0) {
+    resultDiv.innerText = "It's a Draw 📍";
+  }
+  if (score == -1) {
+    scores.computerScore++;
+    resultDiv.innerText = "You Lose 😢";
+  }
+
+  playerScoreDiv.innerText = `Player: ${scores.playerScore}\nComputer: ${scores.computerScore}`;
 }
 
 function onClickRPS(playerChoice) {
-  let computerChoice = getComputerChoice();
-  let score = getResult(playerChoice, computerChoice);
+  const computerChoice = getComputerChoice();
+  const score = getResult(playerChoice, computerChoice);
 
   showResult(score, playerChoice, computerChoice);
 }
 
 function playGame() {
   const rpsButtons = document.querySelectorAll(".rpsButton");
-  console.log(rpsButtons);
 
   rpsButtons.forEach((button) =>
     button.addEventListener("click", () => {
@@ -53,7 +63,15 @@ function playGame() {
 }
 
 function endGame() {
+  scores.playerScore = 0;
+  scores.computerScore = 0;
+
+  const playerScoreDiv = document.getElementById("player-score");
+  const handsDiv = document.getElementById("hands");
   const resultDiv = document.getElementById("result");
+
+  playerScoreDiv.innerText = "";
+  handsDiv.innerText = "";
   resultDiv.innerText = "";
 }
 
